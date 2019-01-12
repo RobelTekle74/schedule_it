e89c0fc06b7b090c79482b53528d188c7e3a9228
Pamela Matthews <Pamela@jeans-MacBook-Pro.local>
2019-01-12 14:07:55 -0500

    setup passport local auth

--- app.js
+++ app.js
@@ -14,7 +14,75 @@ app.set('view engine', 'ejs');
 //bcrypt
 const bcrypt = require('bcrypt');
 
-//passport
+app.post('/auth/ownerRegister', function(req, res, next) {
+    var hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    db.query(`INSERT INTO user (email, password, *) VALUES ('${req.body.email}, '${hashedPassword}', '*')`);
+});
+
+
+//passport and login paths
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({secret: 'robeltakesgirlbaths'}));
+
+passport.use(new LocalStrategy({usernameField: 'email'}, function(email, password, done) {
+    db.one(`SELECT * FROM user WHERE email = '${email}'`)
+        .then(function(result) {
+
+            let fetchedPassword = result.password;
+            let isPasswordMatch = bcrypt.compareSync(password, fetchedPassword);
+
+            if(isPasswordMatch) {
+                done(null, result);
+            } else {
+                done(null, false)
+            };
+        }).catch(function(err){
+                done(null, false);
+        });
+}));
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+    done(null, {
+        id: user.id,
+        email: user.email
+    });
+});
+
+passport.deserializeUser(function(cookie, done) {
+    db.query(`SELECT * FROM user WHERE id = '${cookie.id}'`).then(function(user) {
+        done(null, user);
+    });
+});
+
+app.get('/login', function(req, res) {
+    res.render('login');
+})
+
+app.post('/login', passport.authenticate('local', { failureRedirect: '/'}), function(req, res) {
+    if(result.role = 'owner') {
+        res.redirect('/ownerDashboard');
+    } else {
+        res.redirect('/employeeDashboard');
+    }
+})
+
+
+
+//page routes
+
+app.get('/owner_page/createEmployeeAcct', passport.authenticate('local', { failureRedirect: '/'}), function(req, res) {
+    res.render('/owner_page/createEmployeeAcct')
+});
+
+
 
 //This is for testing locally
 function run() {
