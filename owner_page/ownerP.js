654308efa0f056448d3b10c24db7e8236f527e84
Robel Birkneh <robika@Robels-MacBook-Pro.local>
2019-01-12 10:41:54 -0500

    more additions to nav bar, added a drop down for owner tab to lead to two new pages

---
+++ owner_page/ownerP.js
@@ -0,0 +1,99 @@
+// function renderOwnerProfile() {
+//     return `
+//         <h1> Hello Owner.</h1>
+
+//         <div class="container">
+
+//             <nav class="navbar navbar-expand-lg sticky-top navbar-light">
+//                 <a class="navbar-brand" href="../index.html">Schedule It</a>
+//                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//                     <span class="navbar-toggler-icon"></span>
+//                 </button>
+//                 <div class="collapse navbar-collapse" id="navbarNav">
+//                     <ul class="navbar-nav">
+//                     <li class="nav-item">
+//                         <a class="nav-link" href="../index.html">Home <span class="sr-only">(current)</span></a>
+//                     </li>
+//                     <li class="nav-item active">
+//                         <a class="nav-link" href="./owner_page/ownerP.html">Owner <span class="sr-only">(current)</span></a>
+//                     </li>
+//                     <li class="nav-item">
+//                         <a class="nav-link" href="../employee_page/employeeP.html">Employee</a>
+//                     </li>
+//                     </ul>
+//                 </div>
+//             </nav>
+
+//             <div class="input-group mx-auto pt-5 d-inline-flex">
+//                 <input type="text" class="form-control" placeholder="Employee #">
+//                 <input type="text" class="form-control" placeholder="Employee Full Name">
+//                 <input type="text" class="form-control" placeholder="Employee username">
+//                 <div class="input-group-prepend" id="button-addon3">
+//                     <button class="btn btn-success" type="button">Add</button>
+//                 </div>
+//             </div>
+
+//             <div class="table-responsive pt-5">
+
+//                 <table class="table table-striped">
+//                     <thead>
+//                     <tr class="table-header">
+//                         <th scope="col">Employee #</th>
+//                         <th scope="col">Name</th>
+//                         <th scope="col">Monday</th>
+//                         <th scope="col">Tuesday</th>
+//                         <th scope="col">Wednsday</th>
+//                         <th scope="col">Thursday</th>
+//                         <th scope="col">Friday</th>
+//                         <th scope="col">Saturday</th>
+//                         <th scope="col">Sunday</th>
+//                     </tr>
+//                     </thead>
+
+//                     <tbody>
+//                         <tr>
+//                             <th scope="row"><input type="text" class="form-control" placeholder="Employee #"></th>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                         </tr>
+
+//                         <tr>
+//                             <th scope="row"><input type="text" class="form-control" placeholder="Employee #"></th>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                         </tr>
+
+//                         <tr>
+//                             <th scope="row"><input type="text" class="form-control" placeholder="Employee #"></th>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                             <td><input type="text" class="form-control" placeholder="Employee #"></td>
+//                         </tr>
+
+//                     </tbody>
+
+//                 </table>
+
+//             </div>
+
+//         </div> 
+//     `
+
+// }
