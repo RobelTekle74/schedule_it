21d205ab3da7254396e0da040f996798c139d478
Ashley Souvannaraj <ashleysouvannaraj@Asouv.local>
2019-01-12 19:12:07 -0500

    added sequelize

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
 
+const express = require('express'); const mongoose = require('mongoose');  const app = express(); const PORT = process.env.PORT || 3000;  const config = require('./db');  mongoose.connect(config.DB, { useNewUrlParser: true }).then(   () => {console.log('Database is connected') },   err => { console.log('Can not connect to the database'+ err)} );
+
 // models.user.create({id: 0001, role: "owner", name: "Ashley Souvannaraj", email: "asouvannaraj@gmail.com", password: "password", phone: 1234566674})
 //     .then(function (user) {
 //     console.log(user);
