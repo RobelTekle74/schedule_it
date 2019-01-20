ef2cbe1a12fd7a5d11540b7a50024fdef29886a0
Pamela Matthews <Pamela@jeans-MacBook-Pro.local>
2019-01-19 20:35:58 -0500

    error testing

--- app.js
+++ app.js
@@ -1,84 +1,90 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
+const path = require('path');
 
-//We need this to go back and forth from the website, I think
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 //This part is if we want to use EJS for the html
-const path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//bcrypt
-// const bcrypt = require('bcrypt');
 
-// app.post('/owner_page/createEmployeeAcct.html', function(req, res, next) {
-//     var hashedPassword = bcrypt.hashSync(req.body.password, 10);
-//     db.query(`INSERT INTO user (email, password, *) VALUES ('${req.body.email}', '${hashedPassword}', '*')`);
-// });
+router.get('/employee', function(req, res) {
+    res.render('employee')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //passport
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
+// const passport = require('passport');
+// const LocalStrategy = require('passport-local').Strategy;
+// const session = require('express-session');
 
-app.use(session({secret: 'robeltakesgirlbaths'}));
+// app.use(session({secret: 'robeltakesgirlbaths'}));
 
-passport.use(new LocalStrategy({usernameField: 'email'}, function(email, password, done) {
-    db.one(`SELECT * FROM user WHERE email = '${email}'`)
-        .then(function(result) {
+// passport.use(new LocalStrategy({usernameField: 'email'}, function(email, password, done) {
+//     db.one(`SELECT * FROM user WHERE email = '${email}'`)
+//         .then(function(result) {
 
-            let fetchedPassword = result.password;
-            let isPasswordMatch = bcrypt.compareSync(password, fetchedPassword);
+//             let fetchedPassword = result.password;
+//             let isPasswordMatch = bcrypt.compareSync(password, fetchedPassword);
 
-            if(isPasswordMatch) {
-                done(null, result);
-            } else {
-                done(null, false)
-            };
-        }).catch(function(err){
-                done(null, false);
-        });
-}));
-
-
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function(user, done) {
-    done(null, {
-        id: user.id,
-        email: user.email
-    });
-});
-
-passport.deserializeUser(function(cookie, done) {
-    db.query(`SELECT * FROM user WHERE id = '${cookie.id}'`).then(function(user) {
-        done(null, user);
-    });
-});
+//             if(isPasswordMatch) {
+//                 done(null, result);
+//             } else {
+//                 done(null, false)
+//             };
+//         }).catch(function(err){
+//                 done(null, false);
+//         });
+// }));
 
 
-//page routes
-app.get('/', passport.authenticate('local', { failureRedirect: '/'}), function(req, res) {
-    if(result.role = 'owner') {
-        res.redirect('/owner_page/ownerDashboard.html');
-    } else {
-        res.redirect('/employee_page/employeeP.html');
-    }
-})
 
-app.get('/owner_page/ownerDashboard.html', passport.authenticate('local', { failureRedirect: '/'}), function(req, res) {
-    res.render('/owner_page/ownerDashboard.html');
-});
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-app.get('/employee_page/employeeP.html', passport.authenticate('local', { failureRedirect: '/'}), function(req, res) {
-    res.render('/employee_page/employeeP.html');
-});
+// passport.serializeUser(function(user, done) {
+//     done(null, {
+//         id: user.id,
+//         email: user.email
+//     });
+// });
+
+// passport.deserializeUser(function(cookie, done) {
+//     db.query(`SELECT * FROM user WHERE id = '${cookie.id}'`).then(function(user) {
+//         done(null, user);
+//     });
+// });
+
+
+// //page routes
+// app.get('/', passport.authenticate('local', { failureRedirect: '/'}), function(req, res) {
+//     if(result.role = 'owner') {
+//         res.redirect('/owner_page/ownerDashboard.html');
+//     } else {
+//         res.redirect('/employee_page/employeeP.html');
+//     }
+// })
 
 
 
